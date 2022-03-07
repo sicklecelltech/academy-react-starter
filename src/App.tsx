971447c0fee2dd97200015c6ff1e20a8ed10ea2d
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-03-07 15:00:02 -0500

    Did the main body of the webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 //import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
   return (
     <>
       <Header />
+      <MainContent />
       <Footer />
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 function Footer(): JSX.Element {
   return (
     <footer>
-      <h1>
+      <h5>
         {" "}
         Made by Tiffany Wilmot-Simpson @ Academy{" "}
         <small>date: March 07,2022</small>
-      </h1>
+      </h5>
     </footer>
   );
 }
