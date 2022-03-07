4b67bbcac0d9c4574f45b82c110da64debd13827
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-03-07 07:13:59 -0500

    added a basic header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { greet } from "./utils/greet";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+//import { greet } from "./utils/greet";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
